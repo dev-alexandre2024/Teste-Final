bde4807c050f3538956b69b859e823f6810f6df6
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-20 17:54:22 -0300

    Criando arquivos js para realização do teste

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const dados = []
+
+// TELA DE CADASTRO 
+const formCadastro = document.getElementById('form-cadastro')
+const nomeCadastro = document.getElementById('nome')
+const sobrenomeCadastro = document.getElementById('sobrenome')
+const emailCadastro = document.getElementById('email-cadastro')
+const senhaCadastro = document.getElementById('senha-cadastro')
+const btnCadastro = document.getElementById('cadastrar')
+
+// TELA DE LOGIN
+const formLogin = document.getElementById('form-cadastro')
+const nomeLogin = document.getElementById('nome')
+const sobrenomeLogin = document.getElementById('sobrenome')
+const emailLogin = document.getElementById('email-cadastro')
+const senhaLogin = document.getElementById('senha-cadastro')
+const btnLogin = document.getElementById('cadastrar')
