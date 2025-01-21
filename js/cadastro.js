7754dff8afcfeb56d861ba71daac64ee0ac53f92
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-21 11:27:59 -0300

    Melhorando a logica das funções

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,41 +1,38 @@
-// FUNÇÃO RESPONSÁVEL POR VALIDAR SE TODOS OS CAMPOS FORAM PREENCHIDOS COM SUCESSO
-function ValidarCampos(event) {
+function ValidarCampos() {
     var nome = nomeCadastro.value;
     var sobrenome = sobrenomeCadastro.value;
     var email = emailCadastro.value;
     var senha = senhaCadastro.value;
 
-    if(!nome || !sobrenome || !email || !senha) {
-        event.preventDefault()
-        return alert("Todos os campos devem ser preenchidos")
-    } else if(nome.trim() ==='' || sobrenome.trim() ==='' || email.trim() ==='' || senha.trim() ==='') {
-        return alert("Os campos não devem ter apenas espeços vazios.")
+    if (!nome || !sobrenome || !email || !senha) {
+        return alert("Todos os campos devem ser preenchidos");
+    } else if (nome.trim() === '' || sobrenome.trim() === '' || email.trim() === '' || senha.trim() === '') {
+        return alert("Os campos não devem ter apenas espaços vazios.");
     } else {
-        formCadastro.onsubmit()
-        return {
-            nome, sobrenome, email, senha
-        }
+        return { nome, sobrenome, email, senha };
     }
 }
 
-// FUNÇÃO PARA SALVAR DADOS DE CADASTRO DENTRO DO LOCALSTORAGE
 function SalvarDados(event) {
-    const teste = ValidarCampos(event)
+    dadosFormulario = ValidarCampos(event);
 
-    const { nome, sobrenome, email, senha } = teste
+    if (!dadosFormulario) return; 
+    const { nome, sobrenome, email, senha } = dadosFormulario;
 
     const infor = {
         nome, sobrenome, email, senha
-    }
+    };
 
-    dados.push(infor)
-
-    localStorage.setItem('cadastros', JSON.stringify(dados))
+    dados.push(infor);
+    localStorage.setItem('cadastros', JSON.stringify(dados)); 
 }
 
-// APÓS A VALIDAÇÃO DOS CAMPOS, ESSA FUNÇÃO IRÁ CADASTRAR AS INFORMAÇÕES DENTRO DO LOCALSTORAGE
-// function Cadastrar() {}
+btnCadastro.addEventListener('click', function cadastrar(event) {
+    event.preventDefault();
 
-btnCadastro.addEventListener('click', function cadastrar() {
-    SalvarDados()
-})
+    SalvarDados(event);
+
+    if (dadosFormulario) {
+        window.location.href = formCadastro.action;
+    }
+});
