558f91a953180c14141c0f1924236cb93775d2a3
dev-alexandre2024 <alexandrecdsdev@gmail.com>
2025-01-23 15:13:51 -0300

    Atualizando a inicialização das variáveis

--- js/main.js
+++ js/main.js
@@ -3,17 +3,15 @@ var dadosFormulario;
 
 // TELA DE CADASTRO 
 const formCadastro = document.getElementById('form-cadastro')
-const nomeCadastro = document.getElementById('nome')
-const sobrenomeCadastro = document.getElementById('sobrenome')
+const nomeCadastro = document.getElementById('nome-cadastro')
+const sobrenomeCadastro = document.getElementById('sobrenome-cadastro')
 const emailCadastro = document.getElementById('email-cadastro')
 const senhaCadastro = document.getElementById('senha-cadastro')
 const btnCadastro = document.getElementById('cadastrar')
 
 // TELA DE LOGIN
 const formLogin = document.getElementById('form-login')
-const nomeLogin = document.getElementById('nome')
-const sobrenomeLogin = document.getElementById('sobrenome')
 const emailLogin = document.getElementById('email-login')
 const senhaLogin = document.getElementById('senha-login')
-const btnLogin = document.getElementById('entrar')
+const btnLogin = document.getElementById('btn-login')
 
